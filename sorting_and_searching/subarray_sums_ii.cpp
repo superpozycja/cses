41b03d22ddc1807e9e1b865b3b68df5e17b71c2f
superpozycja <anna@superpozycja.net>
2024-11-03 23:50:30 +0100

    solve subarray sums ii

---
+++ sorting_and_searching/subarray_sums_ii.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ui = unsigned int;
+using l = long;
+using ul = unsigned long;
+using ll = long long;
+using ull = unsigned long long;
+
+using vi = vector<int>;
+using vui = vector<ui>;
+using vl = vector<l>;
+using vul = vector<ul>;
+using vll = vector<ll>;
+using vull = vector<ull>;
+
+void solve()
+{
+	int n, x;
+	cin >> n >> x;
+
+	map<ll, int> a;
+	a[0] = 1;
+	ll lst = 0;
+	ll res = 0;
+	for (int i = 0; i < n; i++) {
+		int z;
+		cin >> z;
+		lst += z;
+		res += a[lst - x];
+		a[lst]++;
+	}
+
+	cout << res << "\n";
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	solve();
+}
